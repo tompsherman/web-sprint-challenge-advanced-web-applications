375aea276be87cbcbfce890494bddc2543d2c2d3
unknown <tompsherman@gmail.com>
2020-10-16 14:50:17 -0400

    testing

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,36 +1,39 @@
+//import libraries
 import React from "react";
 import { getByRole, render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 import { useEffect as mockUseEffect } from "../api/useEffect" 
 
+// create mock before test
 jest.mock("../api/useEffect.js")
 
-const colorList= [{
- color: "aliceblue",
- code: {
-   hex: "#f0f8ff"
- },
- id: 1
-},
-{
- color: "limegreen",
- code: {
-   hex: "#99ddbc"
- },
- id: 2
-},
-{
- color: "aqua",
- code: {
-   hex: "#00ffff"
- },
- id: 3
-}]
+
+//set up test
   test("Fetches data and renders the bubbles", async () => {
-
-    // render app 
+    // mock resolved results
+      // mockUseEffect.mockResolvedValueOnce({
+      //   message: [{
+      //     color: "aliceblue",
+      //     code: {
+      //       hex: "#f0f8ff"
+      //     },
+      //     id: 1
+      //    },
+      //    {
+      //     color: "limegreen",
+      //     code: {
+      //       hex: "#99ddbc"
+      //     },
+      //     id: 2
+      //    },
+      //    {
+      //     color: "aqua",
+      //     code: {
+      //       hex: "#00ffff"
+      //     },
+      //     id: 3
+      //    }]
+      // })
       render (<BubblePage />)
-    // fetched data from API
-
-    // component waits for API then renders bubbles returned
   })
+
